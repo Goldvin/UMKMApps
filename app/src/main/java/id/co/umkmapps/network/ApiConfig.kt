d17a5a038573d4952592f10074dcd3d33ac03d5d
Endah <endahkf29@gmai.com>
2024-03-23 15:57:58 +0700

    Tugas Pertemuan 3

---
+++ app/src/main/java/id/co/umkmapps/network/ApiConfig.kt
@@ -0,0 +1,31 @@
+package id.co.umkmapps.network
+
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class ApiConfig {
+companion object{
+    fun getApiService(): ApiService {
+
+        val loggingInterceptor =
+
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://regres.in/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+
+        }
+    }
+}
